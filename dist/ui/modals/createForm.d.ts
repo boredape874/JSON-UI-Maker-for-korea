93cb73ad0962f1b43ca69f4cd6a709ff30a86d2a
Bored baby <boredape8740002@gmail.com>
2026-03-14 17:26:19 +0900

    Update create form type declarations

--- dist/ui/modals/createForm.d.ts
+++ dist/ui/modals/createForm.d.ts
@@ -1,5 +1,7 @@
 interface CreateFormOptions {
-    title?: string;
+    form_name?: string;
+    namespace?: string;
+    title_flag?: string;
     [key: string]: any;
 }
 export declare function createFormModal(): Promise<CreateFormOptions>;
